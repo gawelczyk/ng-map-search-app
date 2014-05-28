deaa0d32918062738799624c79fc21bd4c3eb49d
pwoloszun <piotr.woloszun@gmail.com>
2014-05-28 15:45:35 +0200

    v 1.4

--- js/controllers/MapCtrl.js
+++ js/controllers/MapCtrl.js
@@ -4,14 +4,12 @@ MapSearchApp.controllers.controller("MapCtrl", [
   function($scope, mapSrv) {
     $scope.centerPoint = mapSrv.center();
     $scope.realEstateMarkers = {};
-    $scope.selectedRealEstate = null;
 
     // TODO
 
     $scope.$on('leafletDirectiveMarker.click', function(event, data) {
-      //TODO
-      var obj = mapSrv.findObject(data["markerName"]);
-      mapSrv.highlightMarker(obj);
+      var selectedRealEstate = mapSrv.findObject(data["markerName"]);
+      mapSrv.highlightMarker(selectedRealEstate);
 
       //TODO
     })
